6b4230f6c5e866c2433f13f842d5cb6a923bdd44
GorkemSinirlioglu <gorkemsinirlioglu99@gmail.com>
2021-10-05 23:00:33 +0200

    5 oct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import Row from "./Row";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey World</h1>
+      <Nav />
+      <Banner />
+      <Row
+        title="Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
